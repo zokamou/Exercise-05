42da228fa6b82a4fceb1394233cac9b9abc91040
zfeller <zfeller@ucsc.edu>
2023-12-08 23:33:14 -0800

    exercise 5

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Exercise 05: Tiny Tween Chain
+// Name:
+// Date:
+
+// CODE ARMOR
+'use strict'
+
+// define game object
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    pixelArt: true,
+    width: 800,
+    height: 600,
+    scene: [ Basics ]
+}
+
+const game = new Phaser.Game(config)
+
+// global
+let cursors = null
